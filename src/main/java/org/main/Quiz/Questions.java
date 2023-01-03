030f7d0acac9f23b2833f2552d3e17ca4237a44c
p20100 <71640994+Lilly05@users.noreply.github.com>
2023-01-03 12:00:42 +0100

    added getMoney method

--- src/main/java/org/main/Quiz/Questions.java
+++ src/main/java/org/main/Quiz/Questions.java
@@ -21,27 +21,26 @@ public class Questions implements QuestionsInterface {
         this.questions = getRandomQuestions(10);
     }
 
-
     @Override
     public ArrayList<String[]> getRandomQuestions(int amount) {
         ArrayList<String[]> chosenQuestions = new ArrayList<>();
         ArrayList<String[]> allQuestions = new ArrayList<>();
         Collections.addAll(allQuestions,
-                new String[]{"Do you like cats?", "B"},
-                new String[]{"Do you like dogs?", "C"},
-                new String[]{"Do you like hampter?", "A"},
-                new String[]{"Do you like pigs?", "A"},
-                new String[]{"Do you like guinea?", "B"},
-                new String[]{"Do you like gin tonic?", "C"},
-                new String[]{"Do you like kittens?", "A"},
-                new String[]{"Do you like ponys?", "C"},
-                new String[]{"Do you like birds?", "B"},
-                new String[]{"Do you like fish?", "A"},
-                new String[]{"Do you like puppies?", "C"},
-                new String[]{"Do you like ineichen?", "A"},
-                new String[]{"Do you like sursee?", "B"},
-                new String[]{"Do you like me?", "A"},
-                new String[]{"Do you like anyone?", "B"}
+                new String[]{"Do you like cats?", "A)No B)Yes C)No" ,"B"},
+                new String[]{"Do you like dogs?", "A)No B)No C)Yes" ,"C"},
+                new String[]{"Do you like hampter?", "A)Yes B)No C)No" ,"A"},
+                new String[]{"Do you like pigs?", "A)Yes B)No C)No" ,"A"},
+                new String[]{"Do you like guinea?", "A)No B)Yes C)No" ,"B"},
+                new String[]{"Do you like gin tonic?", "A)No B)No C)Yes" ,"C"},
+                new String[]{"Do you like kittens?", "A)Yes B)No C)No" ,"A"},
+                new String[]{"Do you like ponys?", "A)No B)No C)Yes" ,"C"},
+                new String[]{"Do you like birds?", "A)No B)Yes C)No" ,"B"},
+                new String[]{"Do you like fish?", "A)Yes B)No C)No" ,"A"},
+                new String[]{"Do you like puppies?", "A)No B)No C)Yes" ,"C"},
+                new String[]{"Do you like ineichen?", "A)Yes B)No C)No" ,"A"},
+                new String[]{"Do you like sursee?", "A)No B)Yes C)No" ,"B"},
+                new String[]{"Do you like me?", "A)Yes B)No C)No" ,"A"},
+                new String[]{"Do you like anyone?", "A)No B)Yes C)No" ,"B"}
         );
         for (int i = 0; i < amount; i++){
             Random random = new Random();
--- src/main/java/org/main/Quiz/Quiz.java
+++ src/main/java/org/main/Quiz/Quiz.java
@@ -1,23 +1,30 @@
 package org.main.Quiz;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Quiz {
 
     Questions questions = new Questions();
 
     public Quiz(){
-        printArrayList(questions.questions);
+        System.out.println(getMoney(questions.questions));
     }
 
-    public void printArrayList(ArrayList<String[]> arraylist){
-        for (String[] element : arraylist){
-            System.out.println(element[0]);
-            System.out.println(element[1]);
+    public int getMoney(ArrayList<String[]> questions){
+        Scanner scanner = new Scanner(System.in);
+        String answer = "";
+        int money = 0;
+        for (String[] question : questions){
+            System.out.println(question[0]);
+            System.out.println(question[1]);
+            answer = scanner.next().toUpperCase();
+            if(answer.equals(question[2])){
+                money += 5;
+            }
         }
+        return money;
     }
-
-
 }
 
 
