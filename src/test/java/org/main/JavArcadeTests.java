da1f42d2fe050cb7fc465175ecdc656b0b9728e6
p20100 <71640994+Lilly05@users.noreply.github.com>
2023-01-10 20:44:55 +0100

    documented tests

--- src/test/java/org/main/JavArcadeTests.java
+++ src/test/java/org/main/JavArcadeTests.java
@@ -14,22 +14,31 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class JavArcadeTests {
 
-    @Before
-    public void setup(){
-
-    }
-
+    /**
+     * testAmountOfQuestions
+     * Method to test if the right amount of questions are given back
+     */
     @Test
     public void testAmountOfQuestions(){
         Questions questions = new Questions(5);
         assertEquals(5, questions.getQuestions().size());
     }
+
+    /**
+     * testAmountOfRewards
+     * Method to test if the right rewards according to the amount of JarCoins are given back
+     */
     
     @Test
     public void testAmountOfRewards(){
         Rewards rewards = new Rewards(15);
         assertEquals(9, rewards.getRewards().size());
     }
+
+    /**
+     * testAnswerQuestions
+     * Method to test if the algorithm to check the user's answer is working correctly
+     */
 
     @Test
     public void testAnswerQuestions(){
@@ -40,6 +49,11 @@ public class JavArcadeTests {
         question.add(new String[]{"Will this test be successful?", "A) Yes B) No C) Maybe", "A"});
         assertEquals(5, quiz.answerQuestions(question));
     }
+
+    /**
+     * testChooseReward
+     * Method to check if the reward the user has won is added to the earnedRewards list
+     */
 
     @Test
     public void testChooseReward(){
